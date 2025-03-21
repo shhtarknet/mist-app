7263d74788716b9421931237387b3dfebbeb0d02
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-22 03:39:25 +0530

    fix(app): support noir wasm

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), topLevelAwait()],
+  plugins: [react(), wasm(), topLevelAwait()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react', '@noir-lang/noirc_abi', '@noir-lang/acvm_js']
   },
 });
