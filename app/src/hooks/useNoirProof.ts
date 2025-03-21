d1974588fd5022385b042594e586e4b1f534c2ee
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-22 04:09:18 +0530

    chore(app): cleanup

--- app/src/hooks/useNoirProof.ts
+++ app/src/hooks/useNoirProof.ts
@@ -5,43 +5,17 @@ import { ProgramCompilationArtifacts } from "@noir-lang/noir_wasm"
 import { InputMap, Noir } from '@noir-lang/noir_js';
 import compiledArtifacts from '../circuits/transfer.json';
 
-
-import initNoirC from "@noir-lang/noirc_abi";
-import initACVM from "@noir-lang/acvm_js";
-import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
-import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
-
-interface EmbeddedCurvePoint {
-  x: string;
-  y: string;
-  is_infinite: boolean;
-}
-
-interface SecretValues {
-  priv_key: string;
-  bal: string;
-  amt: string;
-  rnd: string;
-}
-
-interface ReceiverData {
-  pub_key: EmbeddedCurvePoint;
-  bal_ct: EmbeddedCurvePoint[];
-}
-
-interface ProofData {
-  _s: SecretValues;
-  s_pub_key: EmbeddedCurvePoint;
-  sender_bal_ct: EmbeddedCurvePoint[];
-  r: ReceiverData;
-}
+// import initNoirC from "@noir-lang/noirc_abi";
+// import initACVM from "@noir-lang/acvm_js";
+// import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
+// import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
 
 export function useNoirProof() {
-  useEffect(() => {
-    Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))])
-      .then(r => console.log("WASMs loaded", r))
-      .catch(e => console.error("Failed to load WASMs", e));
-  }, []);
+  // useEffect(() => {
+  //   Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))])
+  //     .then(r => console.log("WASMs loaded", r))
+  //     .catch(e => console.error("Failed to load WASMs", e));
+  // }, []);
   const [isGenerating, setIsGenerating] = useState(false);
 
   const generateProof = useCallback(async (params: InputMap) => {
