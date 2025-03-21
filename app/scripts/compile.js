3d731bee479085483c9edf7607cdfaeae55f7826
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-21 21:01:40 +0530

    feat(app): compile to a file

--- app/scripts/compile.js
+++ app/scripts/compile.js
@@ -1,13 +1,23 @@
 import { compile, createFileManager } from "@noir-lang/noir_wasm"
-
-async function getCircuit() {
-	const fm = createFileManager(import.meta.dirname + '/../../circuits/transfer/');
-	const myCompiledCode = await compile(fm);
-	console.log(JSON.stringify(myCompiledCode))
-
-	return myCompiledCode;
-}
+import { writeFile } from 'fs/promises';
 
 (async () => {
-	const circuit = await getCircuit();
+	const args = process.argv.slice(2);
+
+	// Check if a file path was provided
+	if (args.length === 0) {
+		console.error('Please provide an output file path.');
+		console.error('Usage: pnpm node test.js output.txt');
+		process.exit(1);
+	}
+
+	// Get the file path from command-line arguments
+	const outputPath = args[0];
+
+	const fm = createFileManager(import.meta.dirname + '/../../circuits/transfer/');
+	const myCompiledCode = await compile(fm);
+	await writeFile(outputPath, JSON.stringify(myCompiledCode));
+
+	console.log();
+	console.log('\x1b[32m%s\x1b[0m', 'Written to ' + outputPath);  //cyan
 })();
