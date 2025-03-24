cce49449d3cd358d2bc6dcb91850bd577ce28d9c
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-24 06:16:03 +0530

    feat(app): example proof reconstruction

--- app/scripts/compile.js
+++ app/scripts/compile.js
@@ -3,6 +3,44 @@
 
 import { compile, createFileManager } from "@noir-lang/noir_wasm"
 import { writeFile } from 'fs/promises';
+// import from lib
+import { reconstructHonkProof } from '@aztec/bb.js';
+
+// declare helper functions
+function flattenFieldsAsArray(fields) {
+	const flattenedPublicInputs = fields.map(hexToUint8Array);
+	return flattenUint8Arrays(flattenedPublicInputs);
+}
+
+function flattenUint8Arrays(arrays) {
+	const totalLength = arrays.reduce((acc, val) => acc + val.length, 0);
+	const result = new Uint8Array(totalLength);
+
+	let offset = 0;
+	for (const arr of arrays) {
+		result.set(arr, offset);
+		offset += arr.length;
+	}
+
+	return result;
+}
+
+function hexToUint8Array(hex) {
+	const sanitisedHex = BigInt(hex).toString(16).padStart(64, '0');
+
+	const len = sanitisedHex.length / 2;
+	const u8 = new Uint8Array(len);
+
+	let i = 0;
+	let j = 0;
+	while (i < len) {
+		u8[i] = parseInt(sanitisedHex.slice(j, j + 2), 16);
+		i += 1;
+		j += 2;
+	}
+
+	return u8;
+}
 
 (async () => {
 	const args = process.argv.slice(2);
