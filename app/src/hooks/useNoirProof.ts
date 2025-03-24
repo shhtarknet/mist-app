b7716d56166dbd5f6e2b89218af11b4878d47f4a
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-24 06:16:32 +0530

    feat(app): use Noir proof reconstruction

--- app/src/hooks/useNoirProof.ts
+++ app/src/hooks/useNoirProof.ts
@@ -1,9 +1,45 @@
 import { useCallback, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
-import { UltraHonkBackend } from '@aztec/bb.js';
+import { UltraHonkBackend, reconstructHonkProof } from '@aztec/bb.js';
 import { ProgramCompilationArtifacts } from "@noir-lang/noir_wasm"
 import { InputMap, Noir } from '@noir-lang/noir_js';
 import compiledArtifacts from '../circuits/transfer.json';
+
+// declare helper functions
+export function flattenFieldsAsArray(fields) {
+  const flattenedPublicInputs = fields.map(hexToUint8Array);
+  return flattenUint8Arrays(flattenedPublicInputs);
+}
+
+function flattenUint8Arrays(arrays) {
+  const totalLength = arrays.reduce((acc, val) => acc + val.length, 0);
+  const result = new Uint8Array(totalLength);
+
+  let offset = 0;
+  for (const arr of arrays) {
+    result.set(arr, offset);
+    offset += arr.length;
+  }
+
+  return result;
+}
+
+function hexToUint8Array(hex) {
+  const sanitisedHex = BigInt(hex).toString(16).padStart(64, '0');
+
+  const len = sanitisedHex.length / 2;
+  const u8 = new Uint8Array(len);
+
+  let i = 0;
+  let j = 0;
+  while (i < len) {
+    u8[i] = parseInt(sanitisedHex.slice(j, j + 2), 16);
+    i += 1;
+    j += 2;
+  }
+
+  return u8;
+}
 
 // import initNoirC from "@noir-lang/noirc_abi";
 // import initACVM from "@noir-lang/acvm_js";
