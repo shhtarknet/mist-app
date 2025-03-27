2124035c4139013763322b1cb96606eedf7f4511
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-03-27 15:08:35 +0530

    fix(app): compile script proof param

--- app/scripts/compile.js
+++ app/scripts/compile.js
@@ -98,37 +98,33 @@ const transferParams = {
 		"amt": "0x1234",
 		"rnd": "0x030cffca80ca4344e54e436fc5a03ae8e884b8f3edcb780702599e1951e8aa62"
 	},
-	"s_pub_key": {
-		"x": "0x1c0c2cd4806bd818498f4ef58836794d4ab61417de6e61ee198f7de25898e50e",
-		"y": "0x11da5b03a2d88adb959d5f04f8e03455e59e36b68feabd6170930cd7d473ee92",
-		"is_infinite": "0"
-	},
-	"s_bal_ct": [
-		{
-			"x": "0x13e15bc67dbb21616fb9cb47bb4462f39c36d1727df4f1d05e4c18ae9f8f6318",
-			"y": "0x12cdc1e67b00d986285279d79f7b4f276abe22ecda25a83c5e311cb41045d6a5",
-			"is_infinite": "0"
-		}, {
-			"x": "0x2a0401ac9eec19b4f5472f48793db9f20b3946e3eaab8d3ecbbc3ec69bcbe9e5",
-			"y": "0x03c577df3d7845a6f5875e1e1eb8604871ae24a8b650cfd0a4bc70ab55a682e4",
-			"is_infinite": "0"
-		}
-	],
-	"r": {
+	"s": {
 		"pub_key": {
-			"x": "0x0257eb9d702cfdf9ad18ce614cc49eb6ee4a2260f78c6ba88eb0e72789d456a7",
-			"y": "0x1729aef9320951bfdb39ad3c520577666fdabe318ce1c545e081f2be92573342",
-			"is_infinite": "0"
+			"x": "0x1c0c2cd4806bd818498f4ef58836794d4ab61417de6e61ee198f7de25898e50e",
+			"y": "0x11da5b03a2d88adb959d5f04f8e03455e59e36b68feabd6170930cd7d473ee92",
 		},
 		"bal_ct": [
 			{
 				"x": "0x13e15bc67dbb21616fb9cb47bb4462f39c36d1727df4f1d05e4c18ae9f8f6318",
 				"y": "0x12cdc1e67b00d986285279d79f7b4f276abe22ecda25a83c5e311cb41045d6a5",
-				"is_infinite": "0"
+			}, {
+				"x": "0x2a0401ac9eec19b4f5472f48793db9f20b3946e3eaab8d3ecbbc3ec69bcbe9e5",
+				"y": "0x03c577df3d7845a6f5875e1e1eb8604871ae24a8b650cfd0a4bc70ab55a682e4",
+			}
+		],
+	},
+	"r": {
+		"pub_key": {
+			"x": "0x0257eb9d702cfdf9ad18ce614cc49eb6ee4a2260f78c6ba88eb0e72789d456a7",
+			"y": "0x1729aef9320951bfdb39ad3c520577666fdabe318ce1c545e081f2be92573342",
+		},
+		"bal_ct": [
+			{
+				"x": "0x13e15bc67dbb21616fb9cb47bb4462f39c36d1727df4f1d05e4c18ae9f8f6318",
+				"y": "0x12cdc1e67b00d986285279d79f7b4f276abe22ecda25a83c5e311cb41045d6a5",
 			}, {
 				"x": "0x2837ae6b3eb38bb368e52e4f2db26967710d7f4f46a1a963bd53ad59f1617fe8",
 				"y": "0x1583f4b6db398339d0632fd7211c518ee6bf24db94ae0f0635a5760515d57e9c",
-				"is_infinite": "0"
 			}
 		]
 	}
