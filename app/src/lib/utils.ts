b21abaf4cdc4e617b60d24c26594a350fb65db1e
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-04-18 21:39:11 +0400

    fix(app): dummy decryption

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { CipherText } from "./types";
 
 export function decryptBalance(balEnc: CipherText, privateKey: bigint): string {
-	const dummy = BigInt(balEnc.c1.x);
+	const dummy = BigInt(balEnc.c1.x) + 1n;
 	const fmt = ((dummy + privateKey) / dummy + 125n).toString()
 
 	return fmt.substring(0, fmt.length - 2) + '.' + fmt.substring(fmt.length - 2);
