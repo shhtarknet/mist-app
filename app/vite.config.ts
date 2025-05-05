61a095fc2a822ad6f36a2b630b774c4156f946e5
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-05-05 12:22:55 +0300

    chore(app): add custom bb.js

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  server: {
+    fs: {
+      allow: [
+        // '/Users/shramee/www/starknet/confidential-erc20/app',
+        searchForWorkspaceRoot(process.cwd()),
+        '/Users/shramee/www/crypto/aztec-packages/barretenberg/ts', // Add this path
+      ],
+    },
+  },
   optimizeDeps: {
-    exclude: ['lucide-react', '@noir-lang/noirc_abi', '@noir-lang/acvm_js', 'baby-giant-wasm', 'garaga']
+    exclude: ['lucide-react', '@noir-lang/noirc_abi', '@noir-lang/acvm_js', '@aztec/bb.js', 'baby-giant-wasm', 'garaga']
   },
 });
